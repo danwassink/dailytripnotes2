d034a3863e3839bfe8a43dbdae6dd54bf0dacb2a
Dan Wassink <danwassink@mac.com>
2025-08-10 22:02:11 -0400

    Initial Commit

---
+++ Daily Trip notes/Daily_Trip_notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_Trip_notesApp.swift
+//  Daily Trip notes
+//
+//  Created by Dan Wassink on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Daily_Trip_notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Daily Trip notesTests/Daily_Trip_notesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_Trip_notesTests.swift
+//  Daily Trip notesTests
+//
+//  Created by Dan Wassink on 8/10/25.
+//
+
+import Testing
+@testable import Daily_Trip_notes
+
+struct Daily_Trip_notesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
